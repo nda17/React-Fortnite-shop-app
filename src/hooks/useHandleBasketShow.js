1e2a52704088773002252e4671d0d6dd752569cf
nfdmitry <khv1702@gmail.com>
2023-08-08 23:50:58 +0300

    update files stable v.1.0

--- src/hooks/useHandleBasketShow.js
+++ src/hooks/useHandleBasketShow.js
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
-	const useHandleBasketShow = () => {
-		const [isBasketShow, setBasketShow] = useState(false) //Состояние отвечающее за статус отображения корзины
+const useHandleBasketShow = () => {
+	const [isBasketShow, setBasketShow] = useState(false) //Состояние отвечающее за статус отображения корзины
 
-
+	const handleBasketShow = () => {
 		setBasketShow(!isBasketShow)
-
-
-		return { isBasketShow, setBasketShow}
 	}
+
+	return { isBasketShow, setBasketShow }
+}
 
 export { useHandleBasketShow }
